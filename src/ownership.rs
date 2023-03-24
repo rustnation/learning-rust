4ecf706f8cde0a3336171547bf7d7d7e71ad5613
william <william.munozr@gmail.com>
2023-03-23 21:21:37 -0500

    return values and scope

--- src/ownership.rs
+++ src/ownership.rs
@@ -1,6 +1,7 @@
 pub mod string_type;
 pub mod string_clone_heap;
 pub mod functions;
+pub mod return_values_and_scope;
 
 pub fn ownership() {
     // String Type
@@ -11,4 +12,7 @@ pub fn ownership() {
 
     // Moving a Value
     functions::moving_value();
+
+    // Return values and scope
+    return_values_and_scope::return_values_and_scope();
 }
---
+++ src/ownership/return_values_and_scope.rs
@@ -0,0 +1,20 @@
+pub fn return_values_and_scope() {
+    let _s1 = gives_ownership(); // gives_ownership moves its return value to s1
+
+    let s2 = String::from("hallo"); // s2 comes into scope
+
+    let s3 = takes_and_gives_back(s2);  // s2 is move into takes_and_gives_back,
+    // which also moves its return value into s3
+    println!("{s3}");
+}   // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing happens. s2 goes out of
+    // and is dropped.
+
+fn gives_ownership() -> String { // gives_ownership will move
+    let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string // some_string is returned and moves out to the calling function
+}
+
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into scope
+    a_string // a_string is returned and moves out to the calling function
+}
