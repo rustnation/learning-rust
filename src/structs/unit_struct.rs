89896d1813b7aeaa4d2bed2b531335fe4db2e002
william <william.munozr@gmail.com>
2023-03-24 21:53:03 -0500

    add unit structs

--- src/structs/unit_struct.rs
+++ src/structs/unit_struct.rs
@@ -1,5 +1,6 @@
 struct AlwaysEqual;
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -17,6 +18,10 @@ pub fn master() {
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
+
+    // to work, we need to annotate the struct Rectangle with:
+    // #[derive(Debug)]
+    println!("rect1 is {:?}", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
