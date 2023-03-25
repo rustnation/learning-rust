ea6c7e4a3d0626a69fcfde35274f75f5fb08a01f
william <william.munozr@gmail.com>
2023-03-24 21:48:14 -0500

    add unit structs

--- src/structs/unit_struct.rs
+++ src/structs/unit_struct.rs
@@ -1,5 +1,24 @@
 struct AlwaysEqual;
 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 pub fn master() {
     let _subject = AlwaysEqual;
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 70,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
