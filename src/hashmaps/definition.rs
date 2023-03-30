affdfee22808dd6da21f5047e025fb9aa00a18e7
william <william.munozr@gmail.com>
2023-03-30 18:31:14 -0500

    add working with hashmaps

--- src/hashmaps/definition.rs
+++ src/hashmaps/definition.rs
@@ -33,4 +33,54 @@ pub fn master() {
             println!("name is not present");
         }
     }
+
+    // Working with HashMaps
+    scores();
+
+    // Updating a value based on the old value
+    updating_value_based_on_old_value();
+}
+
+fn scores() {
+    println!("--- HashMap Scores ---");
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    scores.insert(String::from("Green"), 77);
+
+    println!("{:?}", scores);
+
+    println!("--- Accessing Values ---");
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("The value of score is: {}", score);
+
+    println!("--- Loop in the HashMap ---");
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    println!("--- Only insert if the key does not already have a value ---");
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("{:?}", scores);
+}
+
+fn updating_value_based_on_old_value() {
+    println!("--- Updating value based on the old value ---");
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("Value of map: {:?}", map);
 }
