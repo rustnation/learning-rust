96b8f03ae0cf93e1717cd37ecf1e622d67a3361a
william <william.munozr@gmail.com>
2023-03-30 07:43:44 -0500

    add strings definition

--- src/strings/definition.rs
+++ src/strings/definition.rs
@@ -1,5 +1,5 @@
 pub fn master() {
-    let data = "This is a first literal string";
+    let data = "This is a first string literal";
 
     // to_string method is available on any type that implements the Display trait
     let s = data.to_string();
@@ -7,7 +7,7 @@ pub fn master() {
     println!("{}", s);
 
     // the method also works on a literal directly
-    let s1 = "This is a second literal string".to_string();
+    let s1 = "This is a second string literal".to_string();
 
     println!("{}", s1);
 }
