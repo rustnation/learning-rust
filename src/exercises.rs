112cefffe9d5581c90176389890c5427e32138b6
william <william.munozr@gmail.com>
2023-03-31 10:21:17 -0500

    add chapter 8 exercises boilerplate code

--- src/exercises.rs
+++ src/exercises.rs
@@ -1,9 +1,12 @@
 pub mod ex03;
+pub mod ex08;
 
 pub fn master(show: bool) {
     if show {
         common::print_title("EXERCISES");
 
         ex03::master(false);
+
+        ex08::master(true);
     }
 }
---
+++ src/exercises/ex08.rs
@@ -0,0 +1,5 @@
+pub fn master(show: bool) {
+    if show {
+        common::print_title("- Chapter 8");
+    }
+}
