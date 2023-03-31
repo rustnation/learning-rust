0b22d60280788775afe463b4c9089b85474c7d92
william <william.munozr@gmail.com>
2023-03-30 23:42:40 -0500

    add convert fahrenheit

--- src/exercises/ex03/convert_fahrenheit_celsius.rs
+++ src/exercises/ex03/convert_fahrenheit_celsius.rs
@@ -1,3 +1,11 @@
 pub fn master() {
     println!("--- Convert Fahrenheit-Celsius ---");
+
+    let f = 26.0;
+    let c = convert_to_celsius(f);
+    println!("{:?} in Celsius is : {:?}", f, c);
+}
+
+fn convert_to_celsius(f: f32) -> f32 {
+    (5 as f32/9 as f32) * (f - 32.0)
 }
