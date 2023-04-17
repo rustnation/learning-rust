bf4f4f65f037eac2e1721086764b35aee9c84d22
william <william.munozr@gmail.com>
2023-04-17 14:34:19 -0500

    add produce other iterators

--- src/tests/definition.rs
+++ src/tests/definition.rs
@@ -127,3 +127,12 @@ fn iterator_sum() {
 
     assert_eq!(total, 28);
 }
+
+#[test]
+fn produce_other_iterators() {
+    let v1 = vec![1, 2, 3, 4, 5, 6, 7];
+
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4, 5, 6, 7, 8]);
+}
