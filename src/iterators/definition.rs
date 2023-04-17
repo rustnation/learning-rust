ea8d62ed108c9d1fa806db76ef64e1602721e29d
william <william.munozr@gmail.com>
2023-04-17 15:17:24 -0500

    add using iterators with closures

--- src/iterators/definition.rs
+++ src/iterators/definition.rs
@@ -1,7 +1,15 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
 pub fn master(show: bool) {
     if show {
         println!("--- ITERATORS ---");
         creating_an_iterator();
+
+        filter_by_size();
     }
 }
 
@@ -12,5 +20,70 @@ fn creating_an_iterator() {
 
     for val in v1_iter {
         println!("Got: {val}");
+    }
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+fn filter_by_size() {
+    println!("\n- Using the filter method with a closure");
+    let other_shoes = vec![
+        Shoe {
+            size: 11,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 14,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 12,
+            style: String::from("boot"),
+        },
+    ];
+
+    let in_my_size = shoes_in_size(other_shoes, 10);
+
+    println!("value of in_my_size: {:?}", in_my_size);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker"),
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot"),
+                },
+            ]
+        );
     }
 }
