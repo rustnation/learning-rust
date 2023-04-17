d66aa59c6c2bed56587bf6b0e827ba290d2acc01
william <william.munozr@gmail.com>
2023-04-17 14:20:06 -0500

    add iterators definition

--- src/iterators/definition.rs
+++ src/iterators/definition.rs
@@ -1,5 +1,16 @@
 pub fn master(show: bool) {
     if show {
         println!("--- ITERATORS ---");
+        creating_an_iterator();
+    }
+}
+
+fn creating_an_iterator() {
+    println!("\n-- Creating and Iterator");
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {val}");
     }
 }
