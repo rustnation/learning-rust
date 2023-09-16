5c7061a721489bd94ab6fee314fa2e7f131c1206
william <william@godeveloper.io>
2023-09-16 12:50:50 -0500

    add results activity

--- src/results.rs
+++ src/results.rs
@@ -1,9 +1,12 @@
 mod definition;
+mod activity;
 
 pub fn master(show: bool) {
     if show {
         println!("\n-- Results");
 
-        definition::master(true);
+        definition::master(false);
+
+        activity::master(true);
     }
 }
---
+++ src/results/activity.rs
@@ -0,0 +1,45 @@
+#[derive(Debug)]
+struct Adult {
+    age: u8,
+    name: String,
+}
+
+impl Adult {
+    fn new(age: u8, name: &str) -> Result<Self, &str> {
+        if age >= 21 {
+            Ok(Self {
+                age,
+                name: name.to_string(),
+            })
+        } else {
+            Err("Age must be at least 21")
+        }
+    }
+}
+
+fn print_adult(adult: &Result<Adult, &str>) {
+    println!("adult: {:?}", adult);
+}
+
+pub fn master(show: bool) {
+    if show {
+        println!("\n--- Result Activity");
+
+        let child = Adult::new(15, "Anita");
+        let adult = Adult::new(21, "Sanjay");
+
+        print_adult(&child);
+        print_adult(&adult);
+
+        match child {
+            Ok(inner_child) => println!("{} is {} years old.", inner_child.name, inner_child.age),
+            Err(e) => println!("error: {}", e),
+        }
+
+        match adult {
+            Ok(inner_adult) => println!("{} is {} years old.", inner_adult.name, inner_adult.age),
+            Err(e) => println!("error: {}", e),
+        }
+
+    }
+}
