5dcc16c034511cc2bb185256e92955ec93565326
william <william@godeveloper.io>
2023-09-19 21:41:40 -0500

    add iterator activity

---
+++ src/iterators/activity.rs
@@ -0,0 +1,15 @@
+pub fn master(show: bool) {
+    if show {
+        println!("\n--- Activity Using Iterators");
+
+        let data: Vec<_> = vec![1, 2, 3, 4, 5, 6, 7]
+            .iter()
+            .map(|num| num * 3)
+            .filter(|num| num > &10)
+            .collect();
+
+        for num in data {
+            println!("{:?}", num);
+        }
+    }
+}
