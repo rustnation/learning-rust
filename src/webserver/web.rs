622ad12a2fd9e37c7e39e5615bb8295dd93559c8
william <william@godeveloper.io>
2023-09-24 11:25:22 -0500

    validating the request and selectively responding 467 ipone

--- src/webserver/web.rs
+++ src/webserver/web.rs
@@ -50,6 +50,7 @@ fn handle_connection(mut stream: TcpStream) {
 }*/
 
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -69,10 +70,22 @@ pub fn master(show: bool) {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
+    let _http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    println!("Request: {:#?}", http_request);
+
+    let status_line = "HTTP/1.1 200 OK";
+
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!(
+        "{status_line}\r\n\
+        Content-Length: {length}\r\n\r\n\
+        {contents}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
