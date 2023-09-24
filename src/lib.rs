073345e57d9c160385a306cdea9173d43a82e970
william <william@godeveloper.io>
2023-09-24 12:16:59 -0500

    sending requests to threads via channels 480 iphone

--- src/lib.rs
+++ src/lib.rs
@@ -69,26 +69,39 @@ pub fn print_title(title: &str) {
 }
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>
 }
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
-            // create some threads and store them in the vector
+        for id in 0..size {
+            workers.push(Worker::new(id));
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, _f: F)
     where
         F: FnOnce() + Send + 'static,
     {}
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
+    }
 }
 
 /*pub struct ThreadPool {
