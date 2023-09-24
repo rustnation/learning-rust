150c2cf98a6629a6626d887d7500b0ce2501e5cd
william <william@godeveloper.io>
2023-09-24 12:31:53 -0500

    cleaning the project until 484 iphone

--- src/webserver/web.rs
+++ src/webserver/web.rs
@@ -1,54 +1,6 @@
 use crate::print_title;
 use crate::ThreadPool;
 
-/*use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
-};
-
-pub fn master(show: bool) {
-    if show {
-        print_title("Single-threaded Webserver");
-
-        let listener = TcpListener::bind("127.0.0.1:7777").unwrap();
-        let pool = ThreadPool::new(4);
-
-        for stream in listener.incoming() {
-            let stream = stream.unwrap();
-            pool.execute(|| {
-                handle_connection(stream);
-            });
-        }
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(7));
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        _ => ("HTTP/1.1 400 NOT FOUND", "404.html"),
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-    let response = format!(
-        "{status_line}\r\n\
-        Content-Length: {length}\r\n\r\n\
-        {contents}"
-    );
-    stream.write_all(response.as_bytes()).unwrap();
-}*/
-
 use std::{
     fs,
     io::{prelude::*, BufReader},
@@ -77,11 +29,7 @@ pub fn master(show: bool) {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader
-        .lines()
-        .next()
-        .unwrap()
-        .unwrap();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
