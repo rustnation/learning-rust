53f189196179bb20c832de9e99e46d418dbbadc9
william <william@godeveloper.io>
2023-10-03 09:08:08 -0500

    add new type pattern activity

---
+++ src/new_type/activity.rs
@@ -0,0 +1,86 @@
+use crate::print_title;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+// Topic: New type pattern
+//
+// Requirements:
+// * Display the selected color of shoes, a shirt, and pants
+// * Create and display at least one of each type of clothes and color
+//
+// Notes:
+// * Create a new type for each clothing item that wraps the Color enum
+//   * Each new type should implement a `new` function
+// * Create a function for each type of clothes (shoes, shirt, pants)
+//   that accepts the new type specific to that type of clothing
+
+#[derive(Debug, EnumIter)]
+enum Color {
+    Black,
+    Blue,
+    Brown,
+    Custom(String),
+    Gray,
+    Green,
+    Purple,
+    Red,
+    White,
+    Yellow,
+}
+
+#[derive(Debug)]
+struct ShirtColor(Color);
+
+impl ShirtColor {
+    fn new(color: Color) -> Self {
+        Self(color)
+    }
+}
+
+#[derive(Debug)]
+struct ShoesColor(Color);
+
+impl ShoesColor {
+    fn new(color: Color) -> Self {
+        Self(color)
+    }
+}
+
+#[derive(Debug)]
+struct PantsColor(Color);
+
+impl PantsColor {
+    fn new(color: Color) -> Self {
+        Self(color)
+    }
+}
+
+fn print_shirt_color(color: ShirtColor) {
+    println!("shirt color = {:?}", color);
+}
+
+fn print_shoe_color(color: ShoesColor) {
+    println!("shoe color = {:?}", color);
+}
+
+fn print_pant_color(color: PantsColor) {
+    println!("pant color = {:?}", color);
+}
+
+
+pub fn master(show: bool) {
+    if show {
+        print_title("New Type Patter Activity");
+
+        for color in Color::iter() {
+            let shirt_color = ShirtColor::new(color);
+            print_shirt_color(shirt_color);
+        }
+
+        let shoe_color = ShoesColor::new(Color::Blue);
+        print_shoe_color(shoe_color);
+
+        let pant_color = PantsColor::new(Color::Gray);
+        print_pant_color(pant_color);
+    }
+}
--- src/new_type/mod.rs
+++ src/new_type/mod.rs
@@ -1,10 +1,13 @@
 use crate::print_title;
 mod demo;
+mod activity;
 
 pub fn master(show: bool) {
     if show {
         print_title("New Type Pattern");
 
         demo::master(false);
+
+        activity::master(false);
     }
 }
