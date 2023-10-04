34fd2dc5b4490f10829a5042be2532901eb7daa2
william <william@godeveloper.io>
2023-10-04 08:16:17 -0500

    updating shadowing

--- src/basics/shadowing.rs
+++ src/basics/shadowing.rs
@@ -1,11 +1,32 @@
+use crate::print_title;
+
 pub fn master(show: bool) {
     if show {
-        let x = 7;
-        println!("--- Shadowing ---");
-        {
-            let x = x * 2;
-            println!("The value of x in the inner scope is: {x}");
-        }
-        println!("The value of x is: {x}");
+        print_title("Shadowing");
+
+        definition();
+
+        transformation();
     }
+}
+
+fn definition() {
+    print_title("Shadowing Definition");
+
+    let x = 7;
+
+    {
+        let x = x * 7;
+        println!("The value of x in the inner scope is: {x}");
+    }
+    println!("The value of x in the outer scope is: {x}");
+}
+
+fn transformation() {
+    print_title("Shadowing Transformation");
+
+    let spaces = "       ";
+    let spaces = spaces.len();
+
+    println!("Now spaces is usize type: {}", spaces);
 }
