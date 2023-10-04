e6cf53265e2ccb770c85dc7df86aaacc619a00c3
william <william@godeveloper.io>
2023-10-04 07:50:02 -0500

    update constants

--- src/basics/constants.rs
+++ src/basics/constants.rs
@@ -5,12 +5,18 @@ const MAX_SPEED: i32 = 9000;
 pub fn master(show: bool) {
     if show {
         print_title("Constants");
-        const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-        println!("The value of const THREE_HOURS_IN_SECONDS: {THREE_HOURS_IN_SECONDS}");
+
+        definition();
 
         let speed = clamp_speed(9001);
         println!("Speed: {}", speed);
     }
+}
+
+fn definition() {
+    print_title("Constants Definition");
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The value of const THREE_HOURS_IN_SECONDS: {THREE_HOURS_IN_SECONDS}");
 }
 
 fn clamp_speed(speed: i32) -> i32 {
