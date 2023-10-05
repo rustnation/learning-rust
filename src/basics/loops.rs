14347a1bdd132dbc0b3a97d35a3656e8918040e0
william <william@godeveloper.io>
2023-10-05 08:05:30 -0500

    working with loops

--- src/basics/loops.rs
+++ src/basics/loops.rs
@@ -2,10 +2,26 @@ pub fn master(show: bool) {
     if show {
         println!("\n--- Loops");
 
+        returning_values_from_loops();
+
         for_loop_elements();
 
         countdown_loop_with_rev();
     }
+}
+
+fn returning_values_from_loops() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is: {result}");
 }
 
 fn for_loop_elements() {
