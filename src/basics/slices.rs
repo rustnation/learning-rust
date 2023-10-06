1bed5db7692cf532f5fcdb7edd2bcf47c6284f8b
william <william@godeveloper.io>
2023-10-06 11:06:08 -0500

    add slice first_word function

--- src/basics/slices.rs
+++ src/basics/slices.rs
@@ -3,5 +3,21 @@ pub fn master(show: bool) {
         println!("--- Slices ---");
         let slice_array: [i32; 100] = [0; 100];
         println!("slice: {:?}", &slice_array[5..8]);
+
+        let s = String::from("hallo willkommen");
+        let word = first_word(&s);
+        println!("The value of word is: {}", word);
     }
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
