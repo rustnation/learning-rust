6ffe097a9f5869c6fa01c6133c9823526620aa69
William Munoz <william@rustdeveloper.net>
2023-10-31 19:43:11 -0500

    add create_integer_values function

--- src/basics/integers.rs
+++ src/basics/integers.rs
@@ -1,7 +1,13 @@
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+
 pub fn master(show: bool) {
     if show {
         println!("-- Integers");
+
         byte_literals();
+
+        create_integer_values();
     }
 }
 
@@ -9,4 +15,13 @@ fn byte_literals() {
     println!("-- Byte Literals");
     let x_ascii = b'x';
     println!("x in ASCII is: {}", x_ascii);
+}
+
+fn create_integer_values() {
+    let a = 10;
+    let b = Box::new(20);
+    let c = Rc::new(Box::new(30));
+    let d = Arc::new(Mutex::new(40));
+
+    println!("a: {:?}, b: {:?}, c: {:?}, d: {:?}", a, b, c, d);
 }
