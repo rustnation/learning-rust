69468b597b14ccbc8c32cabbe405813f9ecf023b
William Munoz <william@rustdeveloper.net>
2023-11-06 11:26:56 -0500

    add iterators partition in strings

--- src/iterators/partition.rs
+++ src/iterators/partition.rs
@@ -5,6 +5,13 @@ pub fn master(show: bool) {
     if show {
         print_title("Partition");
 
+        in_array(false);
+        in_string(false);
+    }
+}
+
+fn in_array(show: bool) {
+    if show {
         let squares = [4, 9, 16, 25, 36, 49, 64];
         println!("squares: {:?}", squares);
 
@@ -13,5 +20,16 @@ pub fn master(show: bool) {
 
         println!("powers_of_two: {:?}", powers_of_two);
         println!("impure: {:?}", impure);
+    }
+}
+
+fn in_string(show: bool) {
+    if show {
+        let (upper, lower): (String, String) = "Great Teacher Onisuka"
+            .chars()
+            .partition(|&c| c.is_uppercase());
+
+        println!("upper: {:?}", upper);
+        println!("lower: {:?}", lower);
     }
 }
