37c9b8226e6ac6509fc5b670da1e421a77960ad0
William Munoz <william@rustdeveloper.net>
2023-11-10 09:06:46 -0500

    add mutable reference

--- src/mutable_reference/definition.rs
+++ src/mutable_reference/definition.rs
@@ -1,3 +1,13 @@
+pub trait AnyExt {
+    fn type_name(&self) -> &'static str;
+}
+
+impl<T> AnyExt for T {
+    fn type_name(&self) -> &'static str {
+        std::any::type_name::<T>()
+    }
+}
+
 // move the value behind a mutable reference
 pub fn replace_with_84(s: &mut Box<i32>) {
     // this is not okay, as *s would be empty
@@ -14,10 +24,31 @@ pub fn replace_with_84(s: &mut Box<i32>) {
 
 pub fn master(show: bool) {
     if show {
+        mut_definition(false);
+
+        mut_reference(false);
+    }
+}
+
+fn mut_definition(show: bool) {
+    if show {
         println!("Mutable Reference");
         let mut s = Box::new(42);
         println!("value of s: {}", s);
         replace_with_84(&mut s);
         println!("value of s: {}", s);
+    }
+}
+
+fn mut_reference(show: bool) {
+    if show {
+        let x = 77;
+        println!("x is of type: {:?}", x.type_name());
+
+        let mut y = &x;
+        println!("y is of type: {:?}", y.type_name());
+
+        let z = &mut y;
+        println!("z is of type: {:?}", z.type_name());
     }
 }
