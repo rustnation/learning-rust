4a4c173728bc40f6a78c5ab2e86dfc38dce2f1e2
William Munoz <william@rustdeveloper.net>
2024-04-01 20:20:25 -0500

    add working with threads

--- src/threads/definition.rs
+++ src/threads/definition.rs
@@ -71,10 +71,13 @@ fn working_with_threads(show: bool) {
     if show {
         println!("-- Working with threads");
 
-        thread::spawn(f);
-        thread::spawn(f);
+        let t1 = thread::spawn(f);
+        let t2 = thread::spawn(f);
 
         println!("Hello from main thread.");
+
+        t1.join().unwrap();
+        t2.join().unwrap();
     }
 }
 
