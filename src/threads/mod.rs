5a5d00542738e58cf66a4a59850f1716be3729c6
William Munoz <william@rustdeveloper.net>
2024-05-29 08:53:06 -0500

    add thread mutexes

--- src/threads/mod.rs
+++ src/threads/mod.rs
@@ -1,11 +1,15 @@
 mod definition;
 mod preventing_race_condition;
+mod mutexes;
+
 pub fn master(show: bool) {
     if show {
         println!("\n-- Threads");
 
-        definition::master(true);
+        definition::master(false);
 
         preventing_race_condition::master(false);
+
+        mutexes::master(false);
     }
 }
