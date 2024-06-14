a56e6d0c70348705398286a5256f3754db51d234
William Munoz <william@rustdeveloper.net>
2024-06-14 16:14:27 -0500

    add implement complex trait

--- src/traits/complex_trait.rs
+++ src/traits/complex_trait.rs
@@ -1,24 +1,43 @@
+use std::fmt::Debug;
+
 struct Monster {
-    health: u32,
+    health: i32,
 }
 
-struct Wizard {}
-struct Ranger {}
+#[derive(Debug)]
+struct Wizard {
+    health: i32,
+}
 
-trait FightClose {
+#[derive(Debug)]
+struct Ranger {
+    health: i32,
+}
+
+trait DisplayHealth {
+    fn health(&self) -> i32;
+}
+
+impl DisplayHealth for Monster {
+    fn health(&self) -> i32 {
+        self.health
+    }
+}
+
+trait FightClose: Debug {
     fn attack_with_sword(&self, opponent: &mut Monster) {
         opponent.health -= 10;
         println!(
-            "Sword attack! Your opponent has {} health left.",
-            opponent.health
+            "Sword attack! Opponent's health {}. You are now at: {:?}",
+            opponent.health, self
         );
     }
 
     fn attack_with_hand(&self, opponent: &mut Monster) {
         opponent.health -= 2;
         println!(
-            "Hand attack! Your opponent has {} health left.",
-            opponent.health
+            "Hand attack! Opponent's health{}. You are now at: {:?}",
+            opponent.health, self
         )
     }
 }
@@ -26,13 +45,13 @@ trait FightClose {
 impl FightClose for Wizard {}
 impl FightClose for Ranger {}
 
-trait FightFromDistance {
+trait FightFromDistance: Debug {
     fn attack_with_bow(&self, opponent: &mut Monster, distance: u32) {
         if distance < 10 {
             opponent.health -= 10;
             println!(
-                "Bow attack! Your opponent has {} health left.",
-                opponent.health
+                "Bow attack! Opponent's health {}. You are now at: {:?}",
+                opponent.health, self
             )
         }
     }
@@ -42,8 +61,8 @@ trait FightFromDistance {
             opponent.health -= 4;
         }
         println!(
-            "Rock attack! Your opponent has {} health left.",
-            opponent.health
+            "Rock attack! Opponent's health {}. You are now at: {:?}",
+            opponent.health, self
         );
     }
 }
@@ -54,14 +73,23 @@ pub fn master(show: bool) {
     if show {
         println!("--- Complex Trait");
 
-        let radagast = Wizard {};
-        let aragorn = Ranger {};
-
+        let radagast = Wizard { health: 70 };
+        let aragorn = Ranger { health: 80 };
         let mut uruk_hai = Monster { health: 40 };
+
+        println!("Initial Health");
+        println!("Radagast's Health: {:?}", radagast.health);
+        println!("Aragorn's Health: {:?}", aragorn.health);
+        println!("Uruk Hai's Health: {:?}", uruk_hai.health);
 
         radagast.attack_with_sword(&mut uruk_hai);
         radagast.attack_with_hand(&mut uruk_hai);
         aragorn.attack_with_bow(&mut uruk_hai, 7);
         aragorn.attack_with_rock(&mut uruk_hai, 7);
+
+        println!("Final Health");
+        println!("Radagast's Health: {:?}", radagast.health);
+        println!("Aragorn's Health: {:?}", aragorn.health);
+        println!("Uruk Hai's Health: {:?}", uruk_hai.health());
     }
 }
