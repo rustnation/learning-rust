ab550a554405384c6c29ec0c93a6b291d4c14dd4
William Munoz <william@rustdeveloper.net>
2024-06-18 18:56:57 -0500

    add channels definition
    exploring how to work with channels

---
+++ src/channels/definition.rs
@@ -0,0 +1,26 @@
+use std::sync::mpsc::channel;
+use std::thread;
+
+use num::range;
+
+pub fn master(show: bool) {
+    if show {
+        println!("--- Channels Definition\n");
+
+        let (sender, receiver) = channel();
+
+        // spawn off an expensive computation
+        thread::spawn(move || {
+            sender.send(expensive_computation()).unwrap();
+        });
+
+        // let's see what that answer was
+        println!("{:?}", receiver.recv().unwrap());
+    }
+}
+
+fn expensive_computation() {
+    for n in range(0, 1_000_000_000) {
+        println!("The value of n: {n}");
+    }
+}
--- src/channels/mod.rs
+++ src/channels/mod.rs
@@ -1,7 +1,11 @@
 use crate::print_title;
+pub mod definition;
 
 pub fn master(show: bool) {
     if show {
         print_title("Channels");
+
+        // channels definition
+        definition::master(false);
     }
 }
