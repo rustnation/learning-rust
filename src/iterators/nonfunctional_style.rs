54d37223e42fe594a9c497ef9d288baca247fc5e
William Munoz <william@rustdeveloper.net>
2024-06-24 15:59:47 -0500

    add non-functional style
    add non-functional style to iterate

--- src/iterators/nonfunctional_style.rs
+++ src/iterators/nonfunctional_style.rs
@@ -1,5 +1,18 @@
 pub fn master(show: bool) {
     if show {
         println!("Non-functional style");
+
+        let mut new_vec = Vec::new();
+        let mut counter = 1;
+
+        loop {
+            new_vec.push(counter);
+            counter += 1;
+            if counter == 10 {
+                break;
+            }
+        }
+
+        println!("{new_vec:?}");
     }
 }
