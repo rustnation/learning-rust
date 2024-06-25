b3c6cbcd6e763c82b6fa46097cd3fcbb502a5314
william <william.munozr@gmail.com>
2024-06-24 20:52:35 -0500

    add chain iterators
    without for iterator

--- src/iterators/mod.rs
+++ src/iterators/mod.rs
@@ -7,6 +7,7 @@ mod iter_in_collections;
 mod nonfunctional_style;
 mod partition;
 mod scan_hash_table;
+mod without_for;
 
 pub fn master(show: bool) {
     if show {
@@ -29,5 +30,7 @@ pub fn master(show: bool) {
         nonfunctional_style::master(false);
 
         functional_style::master(false);
+
+        without_for::master(false);
     }
 }
