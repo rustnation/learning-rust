7b1747017064228802c9c94ce77231b7244733d1
william <william.munozr@gmail.com>
2024-06-24 22:27:17 -0500

    commenting without_for iterator
    clarifying the use of into_iter

--- src/iterators/without_for.rs
+++ src/iterators/without_for.rs
@@ -8,6 +8,8 @@ pub fn master(show: bool) {
 
         let vector1_a = vector1.iter().map(|x| x + 1).collect::<Vec<i32>>();
 
+        // This destroys vector1 because that's what .into_iter() does.
+        // after make vector1_b, we can't use vector1 again.
         let vector1_b = vector1.into_iter().map(|x| x * 10).collect::<Vec<i32>>();
 
         let mut vector2 = vec![10, 20, 30];
