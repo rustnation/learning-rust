52cc89042b0dda22bac52c2cb845db8fb044ab78
William Munoz <william@rustdeveloper.net>
2024-07-06 12:50:38 -0500

    iterator with fold

--- src/iterators/fold.rs
+++ src/iterators/fold.rs
@@ -1,5 +1,14 @@
 pub fn master(show: bool) {
     if show {
         println!("Iterator with fold");
+
+        let some_numbers = vec![10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+        println!(
+            "{}",
+            some_numbers
+                .iter()
+                .fold(0, |total_so_far, next_number| total_so_far + next_number)
+        );
     }
 }
