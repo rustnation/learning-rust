5a19e59fc83f41c8b99446d6ed1bdcdf518e41e4
William Munoz <william@rustdeveloper.net>
2024-07-07 16:45:03 -0500

    add thread leaking
    thread leakin

--- src/rust_atomics_and_locks/chapter01/thread_leaking.rs
+++ src/rust_atomics_and_locks/chapter01/thread_leaking.rs
@@ -1,5 +1,12 @@
+use std::thread;
+
 pub fn master(show: bool) {
     if show {
         println!("Thread Leaking");
+
+        let x: &'static [i32; 3] = Box::leak(Box::new([1, 2, 3]));
+
+        thread::spawn(move || dbg!(x));
+        thread::spawn(move || dbg!(x));
     }
 }
