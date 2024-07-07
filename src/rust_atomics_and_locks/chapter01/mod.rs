6bfd97ed25a168908a1422aba95a5665bce87251
William Munoz <william@rustdeveloper.net>
2024-07-07 12:17:00 -0500

    add thread result
    capturing result from a thread

--- src/rust_atomics_and_locks/chapter01/mod.rs
+++ src/rust_atomics_and_locks/chapter01/mod.rs
@@ -1,6 +1,7 @@
 mod thread_basics;
 mod thread_closure;
 mod thread_join;
+mod thread_result;
 
 pub fn master(show: bool) {
     if show {
@@ -8,6 +9,7 @@ pub fn master(show: bool) {
 
         thread_basics::master(false);
         thread_join::master(false);
-        thread_closure::master(true);
+        thread_closure::master(false);
+        thread_result::master(false);
     }
 }
---
+++ src/rust_atomics_and_locks/chapter01/thread_result.rs
@@ -0,0 +1,19 @@
+use std::thread;
+
+pub fn master(show: bool) {
+    if show {
+        println!("Thread Result");
+
+        let numbers = Vec::from_iter(0..=1000);
+
+        let t = thread::spawn(move || {
+            let len = numbers.len();
+            let sum = numbers.iter().sum::<usize>();
+            sum / len
+        });
+
+        let average = t.join().unwrap();
+
+        println!("average: {average}");
+    }
+}
