183fe948ce633bb4621b4e1529df2df6284c4e73
William Munoz <william@rustdeveloper.net>
2024-07-12 18:09:57 -0500

    add comment to string literals

--- src/strings/string_literals.rs
+++ src/strings/string_literals.rs
@@ -2,6 +2,9 @@ pub fn master(show: bool) {
     if show {
         println!("String Literals");
 
+        // String Literals
+        // You make this when you write let my_str = ...
+        // They last for the whole program because they are written directly into the binary.
         let my_str = "I am a &str";
         println!("my_str: {my_str:}");
     }
