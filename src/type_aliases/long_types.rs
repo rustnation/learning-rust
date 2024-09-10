d8f86666c2405b25c3263b33c6c7b34ef2d9a243
William Munoz <william@rustdeveloper.net>
2024-09-09 19:09:13 -0500

    add alias for a long type

---
+++ src/type_aliases/long_types.rs
@@ -0,0 +1,16 @@
+type SkipFourTakeFive = std::iter::Take<std::iter::Skip<std::vec::IntoIter<char>>>;
+
+fn returns_some_chars(input: Vec<char>) -> SkipFourTakeFive {
+    input.into_iter().skip(4).take(5)
+}
+
+pub fn master(show: bool) {
+    if show {
+        println!("---Long Types");
+
+        let vector = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+        let skip_four_take_five = returns_some_chars(vector);
+
+        println!("skip_four_take_file: {:?}", skip_four_take_five);
+    }
+}
--- src/type_aliases/mod.rs
+++ src/type_aliases/mod.rs
@@ -1,10 +1,13 @@
 use crate::print_title;
 mod demo;
+mod long_types;
 
 pub fn master(show: bool) {
     if show {
         print_title("Type Aliases");
 
         demo::master(false);
+
+        long_types::master(false);
     }
 }
