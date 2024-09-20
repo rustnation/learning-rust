bd5027a053488ed0e2f5489a4739df764e839c15
William Munoz <william@rustdeveloper.net>
2024-09-20 07:37:50 -0500

    add scoped threads

--- src/threads/mod.rs
+++ src/threads/mod.rs
@@ -3,6 +3,7 @@ mod message_passing_between_threads;
 mod move_data_between_threads;
 mod mutexes;
 mod preventing_race_condition;
+mod scoped_threads;
 mod sharing_data_between_threads;
 mod threads_in_array;
 
@@ -23,5 +24,7 @@ pub fn master(show: bool) {
         move_data_between_threads::master(false);
 
         threads_in_array::master(false);
+
+        scoped_threads::master(false);
     }
 }
---
+++ src/threads/scoped_threads.rs
@@ -0,0 +1,24 @@
+use std::sync::Mutex;
+use std::thread;
+
+pub fn master(show: bool) {
+    if show {
+        println!("---Scoped Threads---");
+
+        let my_number = Mutex::new(0);
+
+        thread::scope(|s| {
+            s.spawn(|| {
+                for _ in 0..10 {
+                    *my_number.lock().unwrap() += 1;
+                }
+            });
+
+            s.spawn(|| {
+                for _ in 0..10 {
+                    *my_number.lock().unwrap() += 1;
+                }
+            });
+        })
+    }
+}
