abee11d8f53e690e191d7e7550f00171afa5bf3e
William Munoz <william@rustdeveloper.io>
2024-12-11 21:30:13 -0500

    add a loop

--- src/loops/definition.rs
+++ src/loops/definition.rs
@@ -2,19 +2,37 @@ pub fn master(show: bool) {
     if show {
         println!("-- Loops");
 
-        loop_print_numbers_1_to_4();
+        loop_print_numbers_1_to_4(false);
+
+        loop_practice(false);
     }
 }
 
-pub fn loop_print_numbers_1_to_4() {
-    println!("\n--- Loop to print numbers from 1 to 4");
-    let mut i = 1;
+pub fn loop_print_numbers_1_to_4(show: bool) {
+    if show {
+        println!("\n--- Loop to print numbers from 1 to 4");
+        let mut i = 1;
 
-    loop {
-        println!("The value of i: {}", i);
-        if i == 4 {
-            break;
+        loop {
+            println!("The value of i: {}", i);
+            if i == 4 {
+                break;
+            }
+            i += 1;
         }
-        i += 1;
+    }
+}
+
+pub fn loop_practice(show: bool) {
+    if show {
+        let mut seven = 1;
+
+        loop {
+            if seven > 7 {
+                break;
+            }
+            println!("The value of seven: {}", seven);
+            seven += 1;
+        }
     }
 }
