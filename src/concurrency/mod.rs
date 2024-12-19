d5e46c91f341ed65def84eab7baa75831e6a1df7
William Munoz <william@rustdeveloper.net>
2024-12-19 16:32:37 -0500

    add sharing state concurrently

--- src/concurrency/mod.rs
+++ src/concurrency/mod.rs
@@ -1,10 +1,12 @@
 mod definition;
 mod message_passing;
+mod sharing_state_concurrently;
 
 pub fn master(show: bool) {
     if show {
         println!("--- Concurrency");
         definition::master(false);
-        message_passing::master(true);
+        message_passing::master(false);
+        sharing_state_concurrently::master(true);
     }
 }
