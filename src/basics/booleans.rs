354b16c1aca32d31b4cba889eb6c183519102d76
William Munoz <william@rustdeveloper.io>
2025-02-09 15:25:07 -0500

    add more boolean examples

--- src/basics/booleans.rs
+++ src/basics/booleans.rs
@@ -7,5 +7,16 @@ pub fn master(show: bool) {
 
         let f = false;
         println!("value of f is: {f}");
+
+        let age = 50;
+        if age <= 20 {
+            println!("the person is young");
+        } else if age > 20 || age <= 35 {
+            println!("the person is in the middle age");
+        } else if age > 35 || age <= 50 {
+            println!("the person is mature");
+        } else if age > 50 {
+            println!("the person is getting old");
+        }
     }
 }
