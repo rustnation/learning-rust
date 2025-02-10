29e04af9592904b62424735a997f18e3e456de18
William Munoz <william@rustdeveloper.io>
2025-02-10 15:01:38 -0500

    add array example

--- src/basics/arrays.rs
+++ src/basics/arrays.rs
@@ -11,6 +11,10 @@ pub fn master(show: bool) {
         type_size(false);
 
         initial_values(false);
+
+        basic_example(false);
+
+        array_index_position(false);
     }
 }
 
@@ -61,5 +65,34 @@ fn initial_values(show: bool) {
 
         let a = [7; 7];
         println!("Values of a: {:?}", a);
+    }
+}
+
+fn basic_example(show: bool) {
+    if show {
+        let numbers = [4, 8, 15, 16, 23, 42];
+
+        for number in numbers {
+            println!("number: {number}");
+        }
+
+        println!("numbers lenght: {}", numbers.len());
+    }
+}
+
+fn array_index_position(show: bool) {
+    if show {
+        let mut seasons = ["Spring", "Summer", "Fall", "Winter"];
+
+        println!("Spring: {}", seasons[0]);
+        println!("Summer: {}", seasons[1]);
+        println!("Fall: {}", seasons[2]);
+        println!("Winter: {}", seasons[3]);
+
+        seasons[2] = "Autumn";
+
+        for season in seasons {
+            println!("season: {}", season);
+        }
     }
 }
