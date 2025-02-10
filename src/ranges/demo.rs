9c4913acaf03a62e50359825ec81398b5651924d
William Munoz <william@rustdeveloper.io>
2025-02-10 16:58:42 -0500

    working with ranges

--- src/ranges/demo.rs
+++ src/ranges/demo.rs
@@ -9,5 +9,31 @@ pub fn master(show: bool) {
         for ch in 'a'..='f' {
             println!("{:?}", ch);
         }
+
+        // month_days is of type RangeInclusive<i32>
+        let month_days = 1..=31;
+
+        for day in month_days {
+            println!("day: {}", day);
+        }
+
+        // grades is of type Range<i32>
+        let grades = 1..6;
+
+        for grade in grades {
+            println!("grade: {}", grade);
+        }
+
+        let letters = 'a'..='z';
+
+        for letter in letters {
+            println!("{letter}");
+        }
+
+        let colors = ["Red", "Green", "Yello"];
+
+        for color in colors {
+            println!("{color}");
+        }
     }
 }
