5b9f9ce0e2fff8a39585190c704bae8d4a1d534f
William Munoz <william@rustdeveloper.io>
2025-02-11 12:55:46 -0500

    defining functions

--- src/functions/definition.rs
+++ src/functions/definition.rs
@@ -1,3 +1,4 @@
+//! src/functions/definition.rs
 fn add(a: i32, b: i32) -> i32 {
     a + b
 }
@@ -11,5 +12,27 @@ pub fn master(show: bool) {
         let z = add(3, 3);
 
         println!("x = {}, y = {}, z = {}", x, y, z);
+
+        open_store(false);
+        bake_pizza(false);
+        swim_in_profit(false);
+    }
+}
+
+fn open_store(show: bool) {
+    if show {
+        println!("Opening my pizza store");
+    }
+}
+
+fn bake_pizza(show: bool) {
+    if show {
+        println!("Baking a pizza");
+    }
+}
+
+fn swim_in_profit(show: bool) {
+    if show {
+        println!("So much $$$, so little time");
     }
 }
--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -1,3 +1,4 @@
+//! src/functions/mod.rs
 mod definition;
 
 pub fn master(show: bool) {
