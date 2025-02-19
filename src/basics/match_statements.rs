1518c84f406b2d86394c03a388ed2c7f2db4c75b
William Munoz <william@rustdeveloper.net>
2025-02-18 20:11:09 -0500

    add match with variables

--- src/basics/match_statements.rs
+++ src/basics/match_statements.rs
@@ -10,5 +10,19 @@ pub fn master(show: bool) {
                 println!("You have not evaluated {}", evaluation);
             }
         }
+
+        match_with_variables(false);
+    }
+}
+
+fn match_with_variables(show: bool) {
+    if show {
+        let number = 7;
+
+        match number {
+            x if x % 2 == 0 => println!("{x} is an even number"),
+            x if x % 2 != 0 => println!("{x} is an odd number"),
+            _ => println!("Unknown"),
+        }
     }
 }
