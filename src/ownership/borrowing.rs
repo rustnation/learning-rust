5decf949a21849ccf8cbfc14e29059c75a0eb4f5
William Munoz <william@rustdeveloper.io>
2025-02-20 15:19:10 -0500

    add notes to borrowing in ownership section

--- src/ownership/borrowing.rs
+++ src/ownership/borrowing.rs
@@ -10,9 +10,13 @@ pub fn master(show: bool) {
     }
 }
 
+// value is borrowing the value of the variable one defined on master function
+// it is not taking ownership of the value of one stored in the heap
 fn print(value: &String) {
     println!("{}", value);
-}
+} // the variables are going to be out of scope
+// value is the owner of memory address that is pointing to the address of one in the heap
+// one is the owner of the memory address on the heap
 
 fn string_type() {
     let mut s = String::from("hello");
