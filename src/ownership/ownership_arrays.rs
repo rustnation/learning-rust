4d11809196ede06f1bc2d96bc70cea767ae522f1
William Munoz <william@rustdeveloper.io>
2025-02-25 18:38:11 -0500

    add ownership in arrays

--- src/ownership/ownership_arrays.rs
+++ src/ownership/ownership_arrays.rs
@@ -2,8 +2,17 @@ pub fn master(show: bool) {
     if show {
         let registrations = [true, false, true];
 
+        // this is valid because the first element of the array is store in stack and implements
+        // copy trait
         let first = registrations[0];
 
-        println!("first value: {first}");
+        println!("first value: {first} and registrations: {registrations:?}");
+
+        let languages = [String::from("Rust"), String::from("Go")];
+
+        // cannot move out of type [String; 2]
+        // let first = languages[0];
+        let first = languages[0].clone();
+        println!("first value: {first}, and languages: {languages:?}");
     }
 }
