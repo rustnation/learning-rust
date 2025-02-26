5e7d29242df6a742111cd45a6bf14b12b10c732f
William Munoz <william@rustdeveloper.io>
2025-02-25 19:14:56 -0500

    add ownership in arrays

--- src/ownership/ownership_arrays.rs
+++ src/ownership/ownership_arrays.rs
@@ -12,7 +12,10 @@ pub fn master(show: bool) {
 
         // cannot move out of type [String; 2]
         // let first = languages[0];
-        let first = languages[0].clone();
+        // this approach is valid but it is not idiomatic in Rust because we are duplicating a
+        // value in the heap
+        // let first = languages[0].clone();
+        let first = &languages[0];
         println!("first value: {first}, and languages: {languages:?}");
     }
 }
