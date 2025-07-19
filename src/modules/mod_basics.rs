2ffdfa3ac9a62fcabb5b82d6564859651136c159
William Munoz <william@rustdeveloper.net>
2025-07-18 21:58:21 -0500

    add mod struct public param private

--- src/modules/mod_basics.rs
+++ src/modules/mod_basics.rs
@@ -1,13 +1,43 @@
 pub fn master(show: bool) {
     if show {
         println!("\n-- Module Basics");
+
+        use print_things::*;
+
+        prints_one_thing("Hello, world!".to_string());
+
+        let my_billy = Billy::new(3);
+        my_billy.print_billy();
     }
 }
 
 mod print_things {
     use std::fmt::Display;
 
-    fn prints_one_thing<T: Display>(input: T) {
+    pub fn prints_one_thing<T: Display>(input: T) {
         println!("{input}");
+    }
+
+    // Billy struct is public, but the parameter name inside it is private.
+    // The user needs to use the public constructor to create an instance.
+    #[derive(Debug)]
+    pub struct Billy {
+        name: String,
+        pub times_to_print: u32,
+    }
+
+    impl Billy {
+        pub fn new(times_to_print: u32) -> Self {
+            Self {
+                name: "Billy".to_string(),
+                times_to_print,
+            }
+        }
+
+        pub fn print_billy(&self) {
+            for _ in 0..self.times_to_print {
+                println!("{}", self.name);
+            }
+        }
     }
 }
