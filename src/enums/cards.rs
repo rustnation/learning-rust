77f00d691faea290f1e59710f9006fccf46ba8fa
William Munoz <william@zen.homelabwm.net>
2025-09-17 19:04:23 -0500

    working with enums

--- src/enums/cards.rs
+++ src/enums/cards.rs
@@ -1,3 +1,37 @@
+#[derive(Debug)]
+enum CardSuit {
+    Hearts,
+    Diamonds,
+    Spades,
+    Clubs,
+}
+
+#[derive(Debug)]
+struct Card {
+    rank: String,
+    suit: CardSuit,
+}
+
 pub fn index(show: bool) {
-    if show {}
+    if show {
+        let first_card = CardSuit::Hearts;
+        println!("first_card: {:?}", first_card);
+
+        let mut second_card = CardSuit::Diamonds;
+        println!("second_card: {:?}", second_card);
+
+        second_card = CardSuit::Spades;
+        println!("second_card: {:?}", second_card);
+
+        second_card = CardSuit::Clubs;
+        println!("second_card: {:?}", second_card);
+
+        let card = Card {
+            rank: "Hearts".to_string(),
+            suit: CardSuit::Hearts,
+        };
+        println!("card value: {:?}", card);
+        println!("card rank: {:?}", card.rank);
+        println!("card suit: {:?}", card.suit);
+    }
 }
