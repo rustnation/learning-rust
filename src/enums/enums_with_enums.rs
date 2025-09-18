9884135a2a61f8887949b3a7815c76d75a638a65
William Munoz <william@zen.homelabwm.net>
2025-09-18 09:28:49 -0500

    add enums with enums and structs

--- src/enums/enums_with_enums.rs
+++ src/enums/enums_with_enums.rs
@@ -1,4 +1,10 @@
 #[derive(Debug)]
+enum Beans {
+    Pinto,
+    Black,
+}
+
+#[derive(Debug)]
 enum Meat {
     Chicken,
     Steak,
@@ -6,20 +12,35 @@ enum Meat {
 
 #[derive(Debug)]
 enum RestaurantItem {
-    Burrito(Meat),
+    Burrito { meat: Meat, beans: Beans },
     Bowl(Meat),
     VeganPlate,
 }
 
 pub fn index(show: bool) {
     if show {
-        let lunch = RestaurantItem::Burrito(Meat::Steak);
+        let mut lunch = RestaurantItem::Burrito {
+            meat: Meat::Steak,
+            beans: Beans::Pinto,
+        };
         let dinner = RestaurantItem::Bowl(Meat::Chicken);
         let abandoned_meal = RestaurantItem::VeganPlate;
 
         println!("lunch: {:?}", lunch);
-        if let RestaurantItem::Burrito(meat) = lunch {
-            println!("lunch add-ons: {:?}", meat);
+        if let RestaurantItem::Burrito { meat, beans } = lunch {
+            println!("lunch meat add-ons: {:?}", meat);
+            println!("lunch beans add-ons: {:?}", beans);
+        }
+
+        lunch = RestaurantItem::Burrito {
+            meat: Meat::Chicken,
+            beans: Beans::Black,
+        };
+
+        println!("lunch variant: {:?}", lunch);
+        if let RestaurantItem::Burrito { meat, beans } = lunch {
+            println!("lunch varian meat add-ons: {:?}", meat);
+            println!("lunch varian beans add-ons: {:?}", beans);
         }
 
         println!("dinner: {:?}", dinner);
