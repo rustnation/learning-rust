88be78c7054c2799dc855514c29c044be8c519a5
William Munoz <william@zen.homelabwm.net>
2025-09-19 15:45:11 -0500

    resolving clippy issue

--- src/add_millimeters_to_meters/definition.rs
+++ src/add_millimeters_to_meters/definition.rs
@@ -1,7 +1,10 @@
 //! src/add_millimeters_to_meters/definition.rs
 use std::ops::Add;
 
+#[derive(Debug)]
 struct Millimeters(u32);
+
+#[derive(Debug)]
 struct Meters(u32);
 
 impl Add<Meters> for Millimeters {
@@ -15,5 +18,13 @@ impl Add<Meters> for Millimeters {
 pub fn master(show: bool) {
     if show {
         println!("-- Implementing the Add trait on Millimeters to add Millimeters and Meters");
+
+        let meters = Meters(7);
+        println!("Meters is: {:?}", meters);
+
+        let millimeters = Millimeters(7000);
+        let mill_returned = millimeters.add(meters);
+
+        println!("The millimeters returned are: {:?}", mill_returned);
     }
 }
