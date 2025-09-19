ab8f8d454ac1aeb06c719de05e58963df9dd5696
William Munoz <william@zen.homelabwm.net>
2025-09-19 14:31:22 -0500

    functions in enums

--- src/enums/enums_and_functions.rs
+++ src/enums/enums_and_functions.rs
@@ -5,11 +5,34 @@ enum LaundryCycle {
     Delicate(String),
 }
 
+impl LaundryCycle {
+    fn wash_laundry(&self) {
+        match self {
+            LaundryCycle::Cold => {
+                println!("Running the laundry with cold temperature");
+            }
+            LaundryCycle::Hot { temperature } => {
+                println!("Running the laundry with a temperature of {temperature}");
+            }
+            LaundryCycle::Delicate(fabric_type) => {
+                println!("Running the laundry with delicate cycle for {fabric_type}");
+            }
+        }
+    }
+}
+
 pub fn index(show: bool) {
     if show {
         wash_laundry(LaundryCycle::Cold);
         wash_laundry(LaundryCycle::Hot { temperature: 70 });
         wash_laundry(LaundryCycle::Delicate(String::from("silk")));
+
+        LaundryCycle::Cold.wash_laundry();
+        let hot_cycle = LaundryCycle::Hot { temperature: 70 };
+        hot_cycle.wash_laundry();
+
+        let delicate_cycle = LaundryCycle::Delicate(String::from("silk"));
+        delicate_cycle.wash_laundry();
     }
 }
 
