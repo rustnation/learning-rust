2b2b9eb215755e161a0cb166db86b19a75d5f8fe
William Munoz <william@zen.homelabwm.net>
2025-09-19 19:25:45 -0500

    add the turbo fish operator

--- src/generics/introduction.rs
+++ src/generics/introduction.rs
@@ -1,5 +1,11 @@
 pub fn index(show: bool) {
     if show {
+        // using TurboFishOperator
+        println!("{}", identity::<i8>(7));
+        println!("{}", identity::<u32>(7));
+        println!("{}", identity::<f64>(7.7));
+
+        // without TurboFishOperator
         println!("{}", identity(7));
         println!("{}", identity(7.7));
         println!("{}", identity("seven"));
