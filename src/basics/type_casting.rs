5f7d1434365a8a18111b78ba9ee8cbebca242503
William Munoz <william@rustdeveloper.rs>
2025-09-27 12:15:01 -0500

    add truncated integer

--- src/basics/type_casting.rs
+++ src/basics/type_casting.rs
@@ -3,5 +3,10 @@ pub fn index(show: bool) {
         let integer_number: i32 = 7;
         let float_number = integer_number as f64;
         println!("float number: {}", float_number);
+
+        // truncated integer
+        let float_number = 7.77;
+        let truncated_integer: i32 = float_number as i32;
+        println!("truncated integer: {}", truncated_integer);
     }
 }
